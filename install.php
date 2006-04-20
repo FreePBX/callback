2b5f690cbd818de636c3c3614f4d4d74a07c9552
Ryan Courtnage <ryan@courtnage.ca>
2006-04-20 15:03:30 +0000

    #682 (Callback modules fails to install after deletion of <webroot>/admin)

--- install.php
+++ install.php
@@ -1,6 +1,6 @@
 <?php
 
-sql('CREATE TABLE IF NOT EXISTS callback ( callback_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY , description VARCHAR( 50 ) , callbacknum VARCHAR( 100 ) , destination VARCHAR( 50 ) , deptname VARCHAR( 50 ));') ;
+sql('CREATE TABLE IF NOT EXISTS callback ( callback_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY , description VARCHAR( 50 ) , callbacknum VARCHAR( 100 ) , destination VARCHAR( 50 ) , sleep INT, deptname VARCHAR( 50 ));') ;
 
 /*
 v1.0, original release
@@ -13,7 +13,14 @@ $callback_installedVersion = modules_getversion($modulename);
 //$callback_installedVersion = modules_getversion('callback');
 
 if (version_compare($callback_installedVersion, "1.1", "<")) {
-	sql('ALTER TABLE callback ADD COLUMN sleep INT DEFAULT 0');
+	
+	// Version 1.1 upgrade
+	$sql = "SELECT sleep FROM callback";
+	$check = $db->getRow($sql, DB_FETCHMODE_ASSOC);
+	if(DB::IsError($check)) {
+		// add new field
+		sql('ALTER TABLE callback ADD COLUMN sleep INT DEFAULT 0');
+	}
 }
 
 ?>
