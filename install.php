fae7a03fa898c37a1f25f0c91393d1f8481ba10d
Rob Thomas <xrobau@gmail.com>
2006-04-21 02:47:10 +0000

    The invisible global $db strikes again.

--- install.php
+++ install.php
@@ -1,5 +1,7 @@
 <?php
 
+global $db;
+
 sql('CREATE TABLE IF NOT EXISTS callback ( callback_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY , description VARCHAR( 50 ) , callbacknum VARCHAR( 100 ) , destination VARCHAR( 50 ) , sleep INT, deptname VARCHAR( 50 ));') ;
 
 /*
