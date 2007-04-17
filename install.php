4d4e55d39e0f3949b04c8fce2b2d4da2b53c5aad
Diego Iastrubni <diego.iastrubni@xorcom.com>
2007-04-17 09:48:52 +0000

    fix to ticket:1793 - sqlite3 support (only sqlite3 and only in freepbx2.3)
    I am using this patch for freepbx 2.2.1, and it does seem to work. if something gets messed up, just email and and I will help you fix this. Tested on 
    sqlite3 and MySQL 5 under Debian Etch. I assume this will work for MySQL 4, and if it's not, I will fix it.

--- install.php
+++ install.php
@@ -1,6 +1,24 @@
 <?php
 
 global $db;
+global $amp_conf;
+
+$autoincrement = ($amp_conf["AMPDBENGINE"] == "sqlite3") ? "AUTOINCREMENT":"AUTO_INCREMENT";
+
+$sql = "CREATE TABLE IF NOT EXISTS callback (
+	callback_id INTEGER NOT NULL PRIMARY KEY $autoincrement,
+	description VARCHAR( 50 ) ,
+	callbacknum VARCHAR( 100 ) ,
+	destination VARCHAR( 50 ) ,
+	sleep INTEGER,
+	deptname VARCHAR( 50 )
+);";
+
+$check = $db->query($sql);
+if (DB::IsError($check)) {
+	die( "Can not create `callback` table: " . $check->getMessage() .  "\n");
+}
+
 
 // Version 1.1 upgrade - add sleep time.
 $sql = "SELECT sleep FROM callback";
--- install.sql
+++ install.sql
@@ -1,1 +1,1 @@
-CREATE TABLE IF NOT EXISTS callback ( callback_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY , description VARCHAR( 50 ) , callbacknum VARCHAR( 100 ) , destination VARCHAR( 50 ) , sleep INT, deptname VARCHAR( 50 ));
+# empty file, for more details see http://freepbx.org/trac/ticket/1277
