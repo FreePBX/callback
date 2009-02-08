3e9cbd383004e335fab3a9a2c4be18d64655d1cd
Micke Carlsson <mickecamino@gmail.com>
2009-02-08 13:27:39 +0000

    Added GPL license text to callback

--- page.callback.php
+++ page.callback.php
@@ -1,15 +1,20 @@
 <?php /* $Id */
 //Copyright (C) 2004 Coalescent Systems Inc. (info@coalescentsystems.ca)
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either version 2
-//of the License, or (at your option) any later version.
+//This file is part of FreePBX.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+//    FreePBX is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    FreePBX is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
 
 isset($_REQUEST['action'])?$action = $_REQUEST['action']:$action='';
 
