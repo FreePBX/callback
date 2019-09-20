a0628fc230961634931c2115e5c98938edef2609
Mohit Mishra <mmishra@sangoma.com>
2019-09-20 08:21:49 +0000

    FREEPBX-20566 Restoring FreePBX 15 Backup is not restoring CallerID and Timeout values of Callback entries.
    	Fixed.

--- Restore.php
+++ Restore.php
@@ -5,7 +5,7 @@ class Restore Extends Base\RestoreBase{
 	public function runRestore(){
 		$configs = $this->getConfigs();
 		foreach ($configs as $callback) {
-			$this->FreePBX->Callback->upsert($callback['callback_id'],$callback['description'],$callback['callbacknum'],$callback['destination'],$callback['sleep'],$callback['deptname']);
+			$this->FreePBX->Callback->upsert($callback['callback_id'],$callback['description'],$callback['callbacknum'],$callback['destination'],$callback['sleep'],$callback['deptname'],$callback['timeout'],$callback['callerid']);
 		}
 	}
 	public function processLegacy($pdo, $data, $tables, $unknownTables){
